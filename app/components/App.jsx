adc654fffb450e82439431c1a92a23e2b04ec2c9
Tatsuya Takamura <takamura@tonchidot.com>
2016-04-04 14:42:13 +0900

    Edit a note

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,5 +1,6 @@
 import uuid  from 'node-uuid';
 import React from 'react';
+import Notes from './Notes.jsx';
 
 export default class App extends React.Component {
     constructor(props) {
@@ -19,9 +20,7 @@ export default class App extends React.Component {
         return (
                 <div>
                   <button onClick={this.addNote}>ADD</button>
-                  <ul>{notes.map(note =>
-                    <li key={note.id}>{note.task}</li>
-                  )}</ul>
+                  <Notes notes={notes} />
                 </div>
         );
     }
--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -1,3 +1,75 @@
 import React from 'react';
 
-export default () => <div>Learn Webpack 33</div>;
+export default class Note extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            editing: false
+        };
+    }
+
+    render (){
+        if (this.state.editing) {
+            return this.renderEdit();
+        }else{
+            return this.renderNote();
+        }
+    }
+
+    renderEdit = () => {
+        // We deal with blur and input handlers here. These map to DOM events.
+        // We also set selection to input end using a callback at a ref.
+        // It gets triggered after the component is mounted.
+        //
+        // We could also use a string reference (i.e., `ref="input") and
+        // then refer to the element in question later in the code. This
+        // would allow us to use the underlying DOM API through
+        // this.refs.input. This can be useful when combined with
+        // React lifecycle hooks.
+        return <input type="text"
+        ref={
+            (e) => e ? e.selectionStart = this.props.task.length : null
+        }
+        autoFocus={true}
+        defaultValue={this.props.task}
+        onBlur={this.finishEdit}
+        onKeyPress={this.checkEnter} />;
+    };
+
+    renderNote = () => {
+        return <div onClick={this.edit}>{this.props.task}</div>;
+    };
+
+    edit = () => {
+        this.setState({
+            editing: true
+        });
+    };
+
+    checkEnter = (e) => {
+        if (e.key === 'Enter') {
+            this.finishEdit(e);
+        }
+    };
+
+    finishEdit = (e) => {
+        // `Note` will trigger an optional `onEdit` callback once it
+        // has a new value. We will use this to communicate the change to
+        // `App`.
+        //
+        // A smarter way to deal with the default value would be to set
+        // it through `defaultProps`.
+        //
+        // See the *Typing with React* chapter for more information.
+        const value = e.target.value;
+
+        if (this.props.onEdit) {
+            this.props.onEdit(value);
+        }
+
+        // Exit edit mode.
+        this.setState({
+            editing: false
+        });
+    };
+}
