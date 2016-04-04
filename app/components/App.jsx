a3bcf0e41aa71e4e00a1d191e546986c053b52cf
Tatsuya Takamura <takamura@tonchidot.com>
2016-04-04 14:19:53 +0900

    Use state

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,8 +1,33 @@
+import uuid  from 'node-uuid';
 import React from 'react';
-import Note  from './Note.jsx';
 
 export default class App extends React.Component {
-    render () {
-        return <Note />;
+    constructor(props) {
+        super(props);
+        this.state = {
+            notes: [
+                {id: uuid.v4(), task: 'Learn Webpack'},
+                {id: uuid.v4(), task: 'Learn React'},
+                {id: uuid.v4(), task: 'Do laundry'}
+            ]
+        };
+        this.addNote = this.addNote.bind(this);
+    }
+
+    render() {
+        const notes = this.state.notes;
+        return (
+                <div>
+                  <button onClick={this.addNote}>ADD</button>
+                  <ul>{notes.map(note =>
+                    <li key={note.id}>{note.task}</li>
+                  )}</ul>
+                </div>
+        );
+    }
+
+    addNote() {
+        this.state.notes.push({id: uuid.v4(), task: 'New Task'});
+        this.setState(this.state)
     }
 }
