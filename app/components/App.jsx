63ee1134078436f4017033ca02d64943b7735c6b
Tatsuya Takamura <takamura@tonchidot.com>
2016-04-05 06:23:02 +0900

    Edit and delete notes

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -12,7 +12,6 @@ export default class App extends React.Component {
                 {id: uuid.v4(), task: 'Do laundry'}
             ]
         };
-        this.addNote = this.addNote.bind(this);
     }
 
     render() {
@@ -20,13 +19,37 @@ export default class App extends React.Component {
         return (
                 <div>
                   <button onClick={this.addNote}>ADD</button>
-                  <Notes notes={notes} />
+                  <Notes notes={notes} onEdit={this.editNote} onDelete={this.deleteNote} />
                 </div>
         );
     }
 
-    addNote() {
+    addNote = () => {
         this.state.notes.push({id: uuid.v4(), task: 'New Task'});
         this.setState(this.state)
-    }
+    };
+
+    editNote = (id, task) => {
+        // Don't modify if trying set an empty value
+        if(!task.trim()) {
+            return;
+        }
+
+        const notes = this.state.notes.map(note => {
+            if(note.id === id && task) {
+                note.task = task;
+            }
+            return note;
+        });
+        this.setState({notes});
+    };
+
+    deleteNote = (id, e) => {
+        // Avoid bubbling to edit
+        e.stopPropagation();
+
+        this.setState({
+            notes: this.state.notes.filter(note => note.id !== id)
+        });
+    };
 }
--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -37,7 +37,17 @@ export default class Note extends React.Component {
     };
 
     renderNote = () => {
-        return <div onClick={this.edit}>{this.props.task}</div>;
+        const onDelete = this.props.onDelete;
+        return (
+            <div onClick={this.edit}>
+                <span>{this.props.task}</span>
+                {onDelete ? this.renderDelete() : null}
+            </div>
+        );
+    };
+
+    renderDelete = () => {
+        return <button onClick={this.props.onDelete}>X</button>
     };
 
     edit = () => {
