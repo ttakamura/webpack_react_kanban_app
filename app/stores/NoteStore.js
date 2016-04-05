24ca36c2d6344604c102033a77d5567dff8b273e
Tatsuya Takamura <takamura@tonchidot.com>
2016-04-06 06:45:35 +0900

    Create alt actions and the store

---
+++ app/stores/NoteStore.js
@@ -0,0 +1,34 @@
+import uuid from 'note-uuid';
+import alt from '../libs/alt';
+import NoteActions from '../actions/NoteActions';
+
+class NoteStore {
+    constructor() {
+        this.bindActions(NoteActions);
+        this.notes = [];
+    }
+    create(note) {
+        const notes = this.notes;
+        note.id = uuid.v4();
+        this.setState({
+            notes: notes.concat(note)
+        });
+    }
+    update(updateNote){
+        const notes = this.notes.map(note => {
+            if (note.id === updateNote.id){
+                return Object.assign({}, note, updatedNote);
+            }else{
+                return note;
+            }
+        });
+        this.setState({notes});
+    }
+    delete(id){
+        this.setState({
+            notes: this.notes.filter(note => note.id !== id)
+        });
+    }
+}
+
+export default alt.createStore(NoteStore, 'NoteStore');
