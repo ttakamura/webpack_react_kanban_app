8fa3d92151b9760f0d44955da4576a6fd7a3790e
Tatsuya Takamura <takamura@tonchidot.com>
2016-04-07 10:49:41 +0900

    Use LocalStorage

--- app/index.jsx
+++ app/index.jsx
@@ -3,5 +3,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './components/App.jsx';
+import alt from './libs/alt';
+import storage from './libs/storage';
+import persistent from './libs/persistent';
+
+persistent(alt, storage, 'myapp');
+
+alt.dispatcher.register(console.log.bind(console));
 
 ReactDOM.render(<App />, document.getElementById('app'));
